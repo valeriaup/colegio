ceae527cd3fb2b16c6e11cc33a54961f77e577bd
Valeria Usuga <usugapenagosvaleria@gmail.com>
2026-02-28 21:39:42 -0500

    feat(index.js) Nuevo endpoint materia.js

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express();
 const port = 3000;
 
 const estudiantesRoute = require('./routes/estudiantes');
-const maestrosRoute = require('./routes/maestros')
+const maestrosRoute = require('./routes/maestros');
+const materiasRoute = require('./routes/materia')
 
 app.use(express.json());
 app.use('/api', estudiantesRoute);
 app.use('/api', maestrosRoute);
+app.use('/api', materiasRoute)
 
 app.listen(port, () => { console.log("Server esta arriba " + port) });
