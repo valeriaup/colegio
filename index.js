69c93c88fa96cf6f1e7950d55ded2854ae658753
Valeria Usuga <usugapenagosvaleria@gmail.com>
2026-02-28 19:44:12 -0500

    feat(index.js) Nuevo endpoint maestros.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const estudiantesRoute = require('./routes/estudiantes')
+const estudiantesRoute = require('./routes/estudiantes');
+const maestrosRoute = require('./routes/maestros')
 
 app.use(express.json());
 app.use('/api', estudiantesRoute);
+app.use('/api', maestrosRoute);
 
 app.listen(port, () => { console.log("Server esta arriba " + port) });
