2ba4abf8b45f9d68049d636ce19a7027d7a43411
Valeria Usuga <usugapenagosvaleria@gmail.com>
2026-03-13 23:36:44 -0500

    feat: modificacion para despliegue Render

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3000;
-
-const estudiantesRoute = require('./routes/estudiantes');
-const maestrosRoute = require('./routes/maestros');
-const materiasRoute = require('./routes/materia');
-const notasRoute = require('./routes/notas')
 
 app.use(express.json());
-app.use('/api', estudiantesRoute);
-app.use('/api', maestrosRoute);
-app.use('/api', materiasRoute);
-app.use('/api', notasRoute);
 
-app.listen(port, () => { console.log("Server esta arriba " + port) });
+const estudiantesRoutes = require('./routes/estudiantes');
+const profesoresRoutes = require('./routes/maestros');
+const materiasRoutes = require('./routes/materia');
+const notasRoutes = require('./routes/notas');
+
+app.use('/Api', estudiantesRoutes);
+app.use('/Api', profesoresRoutes);
+app.use('/Api', materiasRoutes);
+app.use('/Api', notasRoutes);
+
+app.get('/', (req, res) => {
+    res.json({ success: true, message: "Bienvenido a la API del Proyecto 5 - Colegio (SENA)" });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {console.log(`Servidor corriendo en el puerto ${PORT}`);});
